29f13baa48459b23bc7f1c6132bbbb2ca9e3bade
Minsung <alscks6521@naver.com>
2024-04-14 21:13:01 +0900

    first chart default

---
+++ lib/HealthData.dart
@@ -0,0 +1,11 @@
+class HealthData {
+  final int systolicBP;
+  final int diastolicBP;
+  final int bloodSugar;
+
+  HealthData({
+    required this.systolicBP,
+    required this.diastolicBP,
+    required this.bloodSugar,
+  });
+}
